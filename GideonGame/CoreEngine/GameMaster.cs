d90539ec90b036ac2e08db073311917f7055c83e
Josiah Hewitson <JosiahHewitson@gmail.com>
2015-06-11 13:04:52 -0400

    Adding Base stuff

---
+++ GideonGame/CoreEngine/GameMaster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreEngine
+{
+    public class GameMaster
+    {
+        public static void StartGame()
+        {
+            //This is a comment
+        }
+
+        /// <summary>
+        /// This is the method that fires at the end of the game
+        /// </summary>
+        /// <returns>Game over Message</returns>
+        public static string EndGame()
+        {
+            return "Game Over";
+        }
+    }
+}
