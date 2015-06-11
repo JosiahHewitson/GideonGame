c41ab28dcba28103f8c6ea3a4cfe72f09687f31a
Josiah Hewitson <JosiahHewitson@gmail.com>
2015-06-11 14:03:57 -0400

    Adding web Project
    And game state

--- GideonGame/CoreEngine/GameMaster.cs
+++ GideonGame/CoreEngine/GameMaster.cs
@@ -7,9 +7,27 @@ namespace CoreEngine
 {
     public class GameMaster
     {
+        private Objects.GameState _currentState;
+
+        public Objects.GameState CurrentState
+        {
+            get { return _currentState; }
+        }
+
+
+
         public static void StartGame()
         {
             //This is a comment
+        }
+
+        public Objects.GameState ProcessTurn()
+        {
+            Objects.GameState currentState = CurrentState;
+
+            //todo: add update logic
+
+            return currentState;
         }
 
         /// <summary>
---
+++ GideonGame/CoreEngine/Objects/GameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CoreEngine.Objects
+{
+    public class GameState
+    {
+
+
+    }
+}
